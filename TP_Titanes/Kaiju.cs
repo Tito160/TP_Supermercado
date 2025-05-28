18e77cbfea145f06a509afccbdc6400e61278ea1
Tito160 <joeltito160@gmail.com>
2025-05-28 14:43:45 -0300

    Add files via upload

---
+++ TP_Titanes/Kaiju.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 
+{
+    public class Kaiju
+    {
+        public string Nombre {get; set}
+        public string Hanilidad {get; set;}
+        public decimal Peso {get; set;}
+        public int Vida {get; set;}
+        public int Energia {get; set;}
+        public int Tamanio {get; set;}
+        public int Categoria {get; set;}
+
+        public 
+    }
+}
---
+++ TP_Titanes/README.md
@@ -0,0 +1,12 @@
+```mermaid
+classDiagram
+    class Kaiju{
+    - string Nombre
+    - string Habilidad
+    - decimal Peso
+    - int Vida
+    - int Energia
+    - int Tamaño
+    - int Categoria
+    }
+```
